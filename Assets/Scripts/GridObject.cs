ba217a38aab8263ab25d49bf56e05827a96cbf90
Art-i-om <103217156+Art-i-om@users.noreply.github.com>
2024-06-13 13:00:02 +0300

    Initial commit

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,11 @@
+public class GridObject
+{
+    private GridPosition gridPosition;
+    private GridSystem gridSystem;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+    }
+}
